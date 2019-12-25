57a1dd58efb4bf1d97800d2238694c0f8a751475
yiwuxia <740393778@qq.com>
2019-12-25 18:16:06 +0800

    基本走势算法

--- src/main/java/com/lzhpo/core/utils/CalculateUtil.java
+++ src/main/java/com/lzhpo/core/utils/CalculateUtil.java
@@ -1,9 +1,7 @@
 package com.lzhpo.core.utils;
 
 import com.google.common.collect.Lists;
-import org.apache.commons.collections.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
